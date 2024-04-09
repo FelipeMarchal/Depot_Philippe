6904890dcf3a762b127048441a59bdba78de478b
FelipeMarchal <156919811+FelipeMarchal@users.noreply.github.com>
2024-04-09 16:26:27 +0200

    Add files via upload

--- assets/js/code.js
+++ assets/js/code.js
@@ -35,27 +35,17 @@ window.onload = function() {
     });
 }
 function segmentation() {
-	
-	const nbsp = String.fromCharCode(160);
-	
-	const texte = document.getElementById(`fileDisplayArea`).innerText;
-	const delim = document.getElementById(`delimID`).value;
-	
-	if (texte.length > 0) {
 
-	// on remplace tous les caractères de fin de ligne ou tabulation (éventuellement à compléter) par des espaces
-	texte0 = texte.replace(RegExp("(\\n|\\r|\\t)","g"),' ');
+	const texte = document.getElementById(`fileDisplayArea`).innerText;
+	if (texte.length === 0) {alert('Veuillez SVP choisir votre texte !');return}
 	
-	// on ajoute un espace derrière les apostrophes pour les garder dans le texte => "s'", "l'", etc. compteront pour 2 caractères, "qu'" pour 3, etc.
-	texte0 = texte0.replace(/['’]/g,"' ");  
-	
-	// en cas de besoin, on peut mettre en commentaires la ligne suivante
-	texte0 = texte0.toLowerCase();                                                  
-	
-	mots = multipleSplit(texte0, delim); // voir explications fonction ci-dessous
+	const delim = document.getElementById(`delimID`).value;
+	if (delim.length === 0) {alert('Veuillez SVP indiquer vos délimiteurs de mots !');return}
+
+	mots = tokenisation(texte, delim);
 	nMots = mots.length;
 	
-	// on met les mots dans un objet, pour calculer la fréquence des mots en cas de besoin
+	// on met les mots dans un objet, pour calculer la fréquence des mots
 	freqMots = [];
     mots.forEach (m => {if (freqMots[m]) {freqMots[m]++;} else {freqMots[m]=1;}});
 
@@ -68,48 +58,151 @@ function segmentation() {
 		if (lg>0) {
 			fr=freqMots[m]
 			longMots[lg][1] += fr;
-			longMots[lg][2] +=  m + ' ';
+			longMots[lg][2] +=  m + ', ';
 			longMots[lg][3] ++;
 		}
 	};
+	// Début du remplissage de page-analysis que l'on commence par vider-----------------------------------------------------
+	pageAnalysis = document.getElementById("page-analysis");
+	pageAnalysis.innerHTML = "";
 
-	analyse = `Le texte comprend ${nMots} mots.`;
-	
-	analyse += `\n\nListe des mots triés par longueur :`;
-	
-	analyse += '\n+' + '-'.repeat(59) + '+';
-	analyse += '\n|Longueur|Fréquence|' + nbsp.repeat(18) + 'Mots' + nbsp.repeat(18) + '|';
-	analyse += '\n|' + '-'.repeat(59) + '|';
-	
-	/* pour construire un tableau bien colonné, j'ai développé une fonction
-	ajoutant des espaces insécables (Ascii 160) à gauche (nombres) ou à droite (texte),
-	car les espaces simples ( Ascii 32) sont "tassés" dans html;
-    j'ai également chois la police Courier New dans le fichiers css.
-	NB : je n'ai pas réussi à gérer un tableau HTML dans 'page-analysis' en javascript */	
-		
-	longMots.forEach ( lg => {
- 		if (lg[1]>0) {
-			analyse += '\n|'+ ajoutCarac(lg[0], 7, 'gauche', nbsp) + nbsp + '|' + ajoutCarac(lg[1], 8, 'gauche', nbsp) + nbsp + '|' + nbsp;
-			tab_m=lg[2].split(' ');
-			m=''
-			for (i=0;i<tab_m.length;i++) {
-				if (m.length+tab_m[i].length > 32) {
-					analyse += ajoutCarac(m, 38, 'droite', nbsp) + nbsp + '|';
-					m = '';
-					analyse += '\n|'+ nbsp.repeat(8) + '|' + nbsp.repeat(9) + '|' + nbsp;
-				};
-				m+=tab_m[i]+','+nbsp
-			}
-			m = m.slice(0,m.length-4) + ' (' + lg[3] + ')';
-			analyse+=ajoutCarac(m , 38, 'droite', nbsp) + nbsp + '|';
-		};
+	// ligne total :
+	total = document.createElement("h3");
+	total.textContent = "Le texte comprend " + nMots + " mots.";
+	pageAnalysis.appendChild(total);
+
+	// ligne titre du tableau :
+	titre = document.createElement("h3");
+	titre.textContent = "Liste des mots triés par longueur :";
+	pageAnalysis.appendChild(titre);
+
+	// tableau :
+	tableau = document.createElement("table"); tableau.border = "1";
+	entete = document.createElement("tr");
+	colonnes = ["Longueur","Fréquence","Mots"];
+	colonnes.forEach(c => {
+		colonne = document.createElement("th");
+		colonne.textContent = c;
+		entete.appendChild(colonne);
 	});
-	analyse += '\n+' + '-'.repeat(59) + '+';
-		
-	let pageAnalysis = document.getElementById(`page-analysis`);
-	pageAnalysis.innerText = analyse
+	tableau.appendChild(entete);
 
-}}
+	// on remplit le tableau à partir de longMots :
+	longMots.forEach ( lg => {
+		
+ 		if (lg[1]>0) {		
+			longueur=lg[0];freq=lg[1];m=lg[2]
+			m = m.slice(0,m.length-2) + ' ('+ lg[3] + ')';
+			ligTable = document.createElement("tr");
+		
+			cel = document.createElement("td"); cel.align="center"; cel.textContent = longueur; ligTable.appendChild(cel);
+			cel = document.createElement("td"); cel.align="center"; cel.textContent = freq; ligTable.appendChild(cel);
+			cel = document.createElement("td"); cel.textContent = " " + m; ligTable.appendChild(cel);
+
+			tableau.appendChild(ligTable);
+		}
+	});
+	pageAnalysis.appendChild(tableau);
+
+}
+function cooccurrences() {
+
+	const texte = document.getElementById(`fileDisplayArea`).innerText;
+	if (texte.length === 0) {alert('Veuillez SVP choisir votre texte !');return}
+	
+	const delim = document.getElementById(`delimID`).value;
+	if (delim.length === 0) {alert('Veuillez SVP indiquer vos délimiteurs de mots !');return}
+
+	const poleID = document.getElementById(`poleID`).value;
+	if (poleID.length === 0) {alert('Veuillez SVP indiquer votre pôle !');return}
+	
+	const lgID = document.getElementById(`lgID`).value;
+	if (lgID === '0') {alert('Veuillez SVP indiquer une longueur positive !');return}
+	
+	mots = tokenisation(texte, delim);
+	if (!mots.includes(poleID)) {alert('Attention ! Votre pôle ne figure pas dans le texte !');return}	
+	
+	nMots = mots.length;
+
+	// structure de coocc : clé(mot), [fréquence à gauche, fréquence à droite]
+	coocc = []; 
+	for (let i=0;i<nMots;i++){
+		if (mots[i]===poleID){
+			for (j=1;j<=lgID;j++) {
+				if (i-j>0) {
+					m=mots[i-j]
+					if (coocc[m]){coocc[m][0]++} else {coocc[m]=[1,0]}}
+				if (i+j<nMots) {
+					m=mots[i+j]
+					if (coocc[m]){coocc[m][1]++} else {coocc[m]=[0,1]}}
+	}}};
+
+	//pour trier selon la cofréquence décroissante, on transforme l'objet coocc en objet de tableaux
+	// structure de coocc_tri : mot, [cofréquence, fréquence à gauche, fréquence à droite]
+	coocc_tri=[]
+	for (m in coocc) {coocc_tri.push([m, coocc[m][0]+coocc[m][1], coocc[m][0], coocc[m][1]]);}
+	coocc_tri.sort((a, b) => b[1] - a[1]);	
+
+	// Début du remplissage de page-analysis que l'on commence par vider----------------------------------------------------
+	pageAnalysis = document.getElementById("page-analysis");
+	pageAnalysis.innerHTML = "";
+
+	// ligne total :
+	total = document.createElement("h3");
+	total.textContent = "Dans l'intervalle de " + lgID + " mot(s) autour du mot '" + poleID + "', il y a au total " + coocc_tri.length + " mots différents :";
+	pageAnalysis.appendChild(total);
+
+	// tableau :
+	tableau = document.createElement("table"); tableau.border = "1";
+	entete = document.createElement("tr");
+	colonnes = ["Cooccurrent(s)","Co-fréquence","Fréquence gauche","%Fréquence gauche","Fréquence droite","%Fréquence droite"];
+	colonnes.forEach(c => {
+		colonne = document.createElement("th");
+		colonne.textContent = c;
+		entete.appendChild(colonne);
+	});
+	tableau.appendChild(entete);
+
+	// on remplit le tableau à partir de coocc_tri :
+	coocc_tri.forEach ( c => {
+		
+		m=c[0];cofreq=c[1];freq_g=c[2];freq_d=c[3];
+		p_freq_g = Math.round(100*freq_g/cofreq);p_freq_d=100-p_freq_g;
+		
+		ligTable = document.createElement("tr");
+		
+		cel = document.createElement("td"); cel.textContent = " "+m; ligTable.appendChild(cel);
+		cel = document.createElement("td"); cel.align="center"; cel.textContent = cofreq; ligTable.appendChild(cel);
+		cel = document.createElement("td"); cel.align="center"; cel.textContent = freq_g; ligTable.appendChild(cel);
+		cel = document.createElement("td"); cel.align="center"; cel.textContent = p_freq_g+"%"; ligTable.appendChild(cel);
+		cel = document.createElement("td"); cel.align="center"; cel.textContent = freq_d; ligTable.appendChild(cel);
+		cel = document.createElement("td"); cel.align="center"; cel.textContent = p_freq_d+"%"; ligTable.appendChild(cel);
+		
+		tableau.appendChild(ligTable);
+	});
+	pageAnalysis.appendChild(tableau);
+
+}
+function tokenisation(texte, delim, mode = 1, casse = false) {
+	
+	/* mode = 1 : segmentation à partir des séparateurs (délimiteurs)
+	   mode = 2 : segmentation à partir des caractères autorisés dans les mots :
+				  lettres, chiffres, caractères diacritiques (dont accentués et spéciaux français) et caractères grecs */
+				  
+	// on remplace tous les caractères de fin de ligne ou tabulation (éventuellement à compléter) par des espaces
+	texte0 = texte.replace(RegExp("(\\n|\\r|\\t)","g"),' ');
+	
+	// on ajoute un espace derrière les apostrophes pour les garder dans le texte => "s'", "l'", etc. compteront pour 2 caractères, "qu'" pour 3, etc.
+	texte0 = texte0.replace(/['’]/g,"' ");  
+	
+	// d'après le paramètre en entrée 'casse' (true = sensible à la casse)
+	if (!casse) {texte0 = texte0.toLowerCase()};                                                  
+	
+	switch (mode) {
+		case 1 : return multipleSplit(texte0, delim);
+		case 2 : return texte0.match(/['’\wÁ-ɀΑ-ϖ]+/g);
+	}
+}
 function multipleSplit(textString, separators) {
 /* je n'ai pas réussi à mettre au point un regex, à travers la fonction RegExp,
 permettant de faire un split sur plusieurs séparateurs, ceci à cause des caractères génériques;
@@ -126,15 +219,4 @@ j'ai donc développé une fonction sans regex                                   
 	
 	return words;
 
-}
-function ajoutCarac(texte, longueur, cote, carac) {
-	
-	if (cote[0].toLowerCase() === 'g') {
-		texte0=carac.repeat(longueur)+texte;
-		texte0=texte0.slice(texte0.length-longueur);}
-	else {
-		texte0=texte+carac.repeat(longueur);
-		texte0=texte0.slice(0, longueur);}
-
-	return texte0;
 }
